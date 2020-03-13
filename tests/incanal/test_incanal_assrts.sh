5a3dd5063b5126081d8f4c2d9b872707b0ad4590
Pedro Lopez Garcia <pedro.lopez@imdea.org>
2020-03-13 11:32:29 +0100

    Merge.
    Src-commit: e6831138a9cddbe01460b10354b0f961c6d033ae

--- tests/incanal/test_incanal_assrts.sh
+++ tests/incanal/test_incanal_assrts.sh
@@ -6,6 +6,8 @@ _base=$(e=$0;while test -L "$e";do d=$(dirname "$e");e=$(readlink "$e");\
 tests=(trust_C1_+ trust_C1_+bot trust_C2_+ trust_C3_+ trust_C1_- trust_C1_-bot trust_C2_- trust_C3_- trust_S1 trust_S2 trust_S3 trust_S4 trust_C_exported trust_C_internal)
 # TODO: keep up to date w.r.t. test_dirs:inc_trust_call_test/1 and test_dirs:inc_trust_succ_test/1
 
+total_checks=14
+
 if [ "$#" -ne 1 ]; then
           echo "Usage: ./test_incanal_assrts.sh <domain>"
     exit
@@ -20,6 +22,11 @@ trace=trace # comment to remove tracing
 
 # TODO: hardwired directory
 cached_assertions="../../../../build/data/ciaopp_lib_cache"
+mkdir -p "../../../../build/data"
+mkdir -p "../../../../build/data/ciaopp_lib_cache"
+echo "Generating cached assertions for libraries $cached_assertions ..."
+gen_lib_cache $cached_assertions
+
 
 domain=$1
 tag=incanal-assrts
@@ -31,7 +38,6 @@ if [ ! -d "$cached_assertions" ]; then
     echo "Generating cached assertions for libraries $cached_assertions ..."
     gen_lib_cache $cached_assertions
 fi
-
 
 echo "Running tests $domain (monolithic analysis)..."
 
@@ -54,7 +60,10 @@ done
 
 popd > /dev/null 2>&1
 
-echo "$errors BENCHMARKS WITH ERROR(S) FOUND."
+noerrors=$(expr $total_checks - $errors)
+
+echo "$noerrors/$total_checks passed."
+
 
 if [ "$errors" -ne 0 ]; then
     errors=1
