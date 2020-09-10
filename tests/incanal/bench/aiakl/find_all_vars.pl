6ea693c18b8a0f6a893ed6043d9385b85a362781
Isabel Garcia <isabel.garcia@imdea.org>
2020-09-10 16:24:57 +0200

    (ciaopp_tests) aiakl: removed unnecessary recursive dependency
    Src-commit: 5143a3d8cc95319d0505b062de0aefc314fcc1fc

--- tests/incanal/bench/aiakl/find_all_vars.pl
+++ tests/incanal/bench/aiakl/find_all_vars.pl
@@ -6,8 +6,3 @@
 find_all_vars(E,Vars) :-
     find_all_vars2(E,Vars0),
     sort:sort(Vars0,Vars).
-
-append([],A,A).
-append([A|B],C,[A|D]) :-
-    append(B,C,D).
-
--- tests/incanal/bench/aiakl/find_all_vars2.pl
+++ tests/incanal/bench/aiakl/find_all_vars2.pl
@@ -1,9 +1,10 @@
 :- module(find_all_vars2,[find_all_vars2/2],[]).
 
-:- use_module(find_all_vars, [append/3]).
-
 find_all_vars2([],[]).
 find_all_vars2([Vars=_Values|Es],AllVars) :-
     append(Vars,AllVars1,AllVars),
     find_all_vars2(Es,AllVars1).
 
+append([],A,A).
+append([A|B],C,[A|D]) :-
+    append(B,C,D).
