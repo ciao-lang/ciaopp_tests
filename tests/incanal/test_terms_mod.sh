4bcb608f44f380d56b91f1512f6aa02dec6bc853
Isabel Garcia <isabel.garcia@imdea.org>
2020-11-17 10:37:58 +0100

    (ciaopp_tests) update to new command
    Src-commit: d5c679cfc129c86b2e8c4d114d1c4c432074a239

--- tests/incanal/test_terms_mod.sh
+++ tests/incanal/test_terms_mod.sh
@@ -2,9 +2,11 @@
 
 tests=(hanoi aiakl qsort progeom bid rdtok cleandirs prolog_read warplan boyer peephole witt ann manag_proj_simple check_links)
 
+./gen_lib_cache.sh
+
 for i in "${tests[@]}" ; do
-    timeout 300s ./incanal_intermod_bench_driver "$i" del 1 terms --steps 1 --user_tag montypes
-    timeout 300s ./incanal_intermod_bench_driver "$i" del 1 terms modular --steps 1 --user_tag montypes
+    timeout 300s ciaopp-test incanal "$i" terms --edit_type del --steps 0 --user_tag montypes
+    timeout 300s ciaopp-test incanal "$i" terms --edit_type del modular --steps 0 --user_tag montypes
 
     echo "Checking $i ---------"
     ciaopp-dump cmp --no-missing test_results/$i-del-not_rand-1-terms-dd-montypes/mon-noninc-top_down/detailed_step_results/inc_reg_1/*.dump_inc test_results/$i-del-not_rand-1-terms-dd-montypes/mod-noninc-top_down/detailed_step_results/inc_reg_1/ terms
