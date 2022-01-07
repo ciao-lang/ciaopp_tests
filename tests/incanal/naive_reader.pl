8cfaabc37f2f1d9ee6643827263ed5733b4cafac
Jose F. Morales <jfmcjf@gmail.com>
2022-01-07 12:56:29 +0100

    (core) fix '|' operator priority to 1105 (ISO DCG draft)
    Src-commit: b7d65b9826b54ae4cb3130ba1f8820ed104cd6af

--- tests/incanal/naive_reader.pl
+++ tests/incanal/naive_reader.pl
@@ -160,7 +160,7 @@ runtime_ops :-
     op(1150,xfx,(regtype)),
     % dcg
     op(1200, xfx,[(-->)]),
-    op(1100, xfy, ('|')),
+    op(1105, xfy, ('|')),
     % fsyntax
     op(1150,  fx, (function)),
     op(1150,  fx, (fun_eval)),
@@ -169,5 +169,5 @@ runtime_ops :-
     op(  50,  fx, (~)),
     op( 910,  fx, (^^)),
     op(  25,  fy, (^)),
-    op(1100, xfy, ('|')),
+    op(1105, xfy, ('|')),
     op(1050, xfy, (?)).
